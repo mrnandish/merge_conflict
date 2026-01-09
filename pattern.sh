344fe922c594f275ac7cf03ff4f462945bfb6c32
unknown <mrnandish8@gamil.com>
2026-01-09 21:50:23 +0530

    ajdj

--- pattern.sh
+++ pattern.sh
@@ -2,7 +2,7 @@
 grep -ilr "$1" *
 if [ $? -eq 0 ]
 then
-	echo "pattern   nandish"
+	echo "patter   nandish"
 	echo "version 1"
 	echo "version:V1"
 else
@@ -13,7 +13,7 @@ else
 	echo " adding new line to test "
 =======
 	echo" edited on jan 9"
->>>>>>> 4b706800a14e5e4d2794cc7c371773eddd986197
+
 fi
 
 
--- sum.sh
+++ sum.sh
@@ -1,15 +1,7 @@
-<<<<<<< HEAD
-echo "newly edited "
-=======
-<<<<<<< HEAD
+echo "nandish"
 echo "sum of 2 numbers by Nandish"
-=======
 echo "sum of 2 numbers"
-<<<<<<< HEAD
 echo "verion 3 "
-=======
 echo " sum of 3 nos"
->>>>>>> 0458d2298621d1177b1b5206663973f74c636b1f
->>>>>>> 0c11bd3953be598e9b9da31622acbc5fd007fea9
 
 echo "edited on january 9th 2026"
